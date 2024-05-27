40a5a11d6f6eac5d4878542343c1f8b2ab0d5bef
sr931662 <83828392+sr931662@users.noreply.github.com>
2024-05-27 10:32:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ app.use(morgan('dev'))
 app.use('/api/v1/auth', require('./routes/userRoutes'))
 
 // Home
-// app.get("/", (req,res)=>{
-//   res.status(200).send({
-//     "success":true,
-//     "msg":"Node Serevr Running"
-//   })
-// })
+app.get("/", (req,res)=>{
+  res.status(200).send({
+    "success":true,
+    "msg":"Node Serevr Running"
+  })
+})
 
 // PORT
 const PORT = process.env.PORT || 8080
